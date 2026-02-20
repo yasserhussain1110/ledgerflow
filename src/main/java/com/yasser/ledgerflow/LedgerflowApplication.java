6689a1353bdb57a1c0debfccc245fcee3a7e6de5
Yasser Hussain <yasserhussain1110@gmail.com>
2026-02-20 07:39:30 +0530

    Build application scaffold

---
+++ src/main/java/com/yasser/ledgerflow/LedgerflowApplication.java
@@ -0,0 +1,13 @@
+package com.yasser.ledgerflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LedgerflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LedgerflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yasser/ledgerflow/LedgerflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yasser.ledgerflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedgerflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
